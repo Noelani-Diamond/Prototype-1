cadce754b286922429611fd033673e1ebc3c2ebf
Noelani-Diamond <nceballos26@aoiths.org>
2025-03-14 13:12:30 -0400

    This is the mini challenges

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0.3f, 4.53f, -11.46f);
+    Vector3 offset = new Vector3(0, 5, -10);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+      transform.position = player.transform.position + offset;
     }
 }
+
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
-    private float turnSpeed; 
-    private float horizontalInput;
-    private float fowardInput;
+    public float speed = 5;
+    public float turnSpeed = 50;
+    public float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
-        fowardInput= Input.GetAxis("Vertical");
-        // Moves the car foward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
-        // Rotates the car based on horizontal input 
+        verticalInput = Input.GetAxis("Vertical");
+        // Moves the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Turns the car on the Y axis
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
